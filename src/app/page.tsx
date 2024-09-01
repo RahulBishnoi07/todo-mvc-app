04a8fa3b5131b9c239a9c70b78a7a4262b09f0a5
Rahul Bishnoi <rahul.gdr29@gmail.com>
2024-09-01 19:56:21 +0530

    refactor: Improve todo list functionality and UI

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,20 +12,23 @@ export default function Home() {
   const [filter, setFilter] = useState<string>("All");
 
   const handleAddTodo = (input: string) => {
-    const newTodo = { text: input, completed: false };
-    setTodos([...todos, newTodo]);
+    setTodos([...todos, { text: input, completed: false }]);
   };
 
   const handleDeleteTodo = (index: number) => {
-    const updatedTodos = todos.filter((_, i) => i !== index);
-    setTodos(updatedTodos);
+    setTodos(todos.filter((_, i) => i !== index));
   };
 
   const toggleCompleted = (index: number) => {
-    const updatedTodos = todos.map((todo, i) =>
-      i === index ? { ...todo, completed: !todo.completed } : todo
+    setTodos(
+      todos.map((todo, i) =>
+        i === index ? { ...todo, completed: !todo.completed } : todo
+      )
     );
-    setTodos(updatedTodos);
+  };
+
+  const clearCompleted = () => {
+    setTodos(todos.filter((todo) => !todo.completed));
   };
 
   const handleKeyPress = (event: React.KeyboardEvent<HTMLInputElement>) => {
@@ -41,47 +44,51 @@ export default function Home() {
   const filteredTodos = todos.filter((todo) => {
     if (filter === "Active") return !todo.completed;
     if (filter === "Completed") return todo.completed;
-    return true; // "All" filter
+    return true;
   });
 
   return (
-    <main className="flex flex-col items-center h-screen p-4">
-      <div>todos</div>
-      <input
-        id="todoInput"
-        type="text"
-        placeholder="What needs to be done?"
-        className="border border-gray-300 p-2 rounded mt-4 text-black"
-        onKeyPress={handleKeyPress}
-      />
-      <ul className="mt-4 w-full max-w-md">
-        {filteredTodos.map((todo, index) => (
-          <li
-            key={index}
-            className="flex items-center justify-between p-2 border-b border-gray-300"
-          >
-            <div className="flex items-center">
-              <input
-                type="checkbox"
-                checked={todo.completed}
-                onChange={() => toggleCompleted(index)}
-                className="mr-2"
-              />
-              <span className="text-white">{todo.text}</span>
-            </div>
-            <button
-              className="text-red-500"
-              onClick={() => handleDeleteTodo(index)}
+    <main className="flex flex-col items-center h-screen p-10 bg-white">
+      <div className="text-rose-700 text-8xl">todos</div>
+      <div className="box">
+        <input
+          id="todoInput"
+          type="text"
+          placeholder="What needs to be done?"
+          className="border border-rose-700 p-2 rounded mt-4 text-black"
+          onKeyPress={handleKeyPress}
+        />
+        <ul className="mt-4 w-full max-w-md">
+          {filteredTodos.map((todo, index) => (
+            <li
+              key={index}
+              className="flex items-center justify-between p-2 border-b border-rose-700"
             >
-              ✖
-            </button>
-          </li>
-        ))}
-      </ul>
-      <div className="flex space-x-4 mt-4 text-white">
-        <button onClick={() => setFilter("All")}>All</button>
-        <button onClick={() => setFilter("Active")}>Active</button>
-        <button onClick={() => setFilter("Completed")}>Completed</button>
+              <div className="flex items-center">
+                <input
+                  type="checkbox"
+                  checked={todo.completed}
+                  onChange={() => toggleCompleted(index)}
+                  className="mr-2"
+                />
+                <span className="text-black">{todo.text}</span>
+              </div>
+              <button
+                className="text-black"
+                onClick={() => handleDeleteTodo(index)}
+              >
+                ✖
+              </button>
+            </li>
+          ))}
+        </ul>
+        <div className="flex space-x-4 mt-4 text-black">
+          <span>{filteredTodos.length} items left</span>
+          <button onClick={() => setFilter("All")}>All</button>
+          <button onClick={() => setFilter("Active")}>Active</button>
+          <button onClick={() => setFilter("Completed")}>Completed</button>
+          <button onClick={() => clearCompleted()}>Clear Completed</button>
+        </div>
       </div>
     </main>
   );
